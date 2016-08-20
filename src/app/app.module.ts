6941df079be0089292814f0b36d582ddf9d795c3
JackChan <jack5241027@gmail.com>
2016-08-20 10:27:12 +0800

    practice 3 製作header compponent

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  entryComponents: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+
+}
